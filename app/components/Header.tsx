5907cc0d978d3c8ebd456fb968f8914bfe633645
James <james@whereyouat.co.uk>
2024-06-14 22:23:21 +0100

    feat: Add header component to home page
    This commit adds the `Header` component to the home page, improving the overall layout and design. The header component will be displayed at the top of the page, providing a consistent navigation experience for users.

---
+++ app/components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+
+const Header = () => (
+  <header className="bg-gray-100 dark:bg-gray-900 py-4">
+    <div className="container mx-auto px-4 md:px-6 lg:px-8 flex justify-between items-center">
+      <Link
+        href="/"
+        className="text-2xl font-bold text-gray-900 dark:text-white"
+        prefetch={false}
+      >
+        Dr. Nicholas Dimonaco
+      </Link>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              href="/publications"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white relative after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-gray-900 after:dark:bg-white hover:after:w-full after:transition-all after:ease-in-out after:duration-300"
+              prefetch={false}
+            >
+              Publications
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/tools"
+              className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white relative after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-gray-900 after:dark:bg-white hover:after:w-full after:transition-all after:ease-in-out after:duration-300"
+              prefetch={false}
+            >
+              Tools
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+);
+export default Header;
